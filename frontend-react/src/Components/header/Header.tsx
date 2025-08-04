aeb2f81ac0620baab899f2490baac7451d504f38
Lubna-Keran <lubnakiranff@gmail.com>
2025-08-04 17:54:07 +0500

    register/login pages

--- frontend-react/src/Components/header/Header.tsx
+++ frontend-react/src/Components/header/Header.tsx
@@ -6,7 +6,7 @@ import { Container } from 'react-bootstrap';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import Logo from '../../assets/images/logo.png';
 import { useState } from 'react';
 import './header.css'
