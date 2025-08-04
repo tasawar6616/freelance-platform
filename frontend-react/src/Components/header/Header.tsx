2d2d5d945a948e2a0f1495f60d03e4379117d0f7
Lubna-Keran <lubnakiranff@gmail.com>
2025-08-04 17:36:29 +0500

    login/register pages

--- frontend-react/src/Components/header/Header.tsx
+++ frontend-react/src/Components/header/Header.tsx
@@ -1,8 +1,5 @@
-import React from 'react'
 import Button from 'react-bootstrap/Button';
 import { Container } from 'react-bootstrap';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
@@ -23,11 +20,10 @@ const generalNavItems: NavItem[] = [
 const Header = () => {
     const [isNavOpen, setIsNavOpen] = useState(false);
 
-    const handleNavClose = () => setIsNavOpen(false);
-
   const handleToggleClick = () => setIsNavOpen((prev) => !prev);
 
     const navigate = useNavigate();
+
     const navItems: NavItem[] =  generalNavItems;
  return (
    <Navbar expand="lg" className="navWrap custom-header">
@@ -57,8 +53,12 @@ const Header = () => {
             </NavDropdown>
           </Nav>
           <Nav>
-            <Button variant="dark" className="me-2 custom-register">REGISTER</Button>
-            <Button variant="danger" className="custom-signin">SIGN IN</Button>
+            <Button onClick={() => {
+              navigate('/register');
+            }} variant="dark" className="me-2 btn secondary-btn">REGISTER</Button>
+            <Button onClick={() => {
+              navigate('/login');
+            }} variant="danger" className="btn primary-btn">SIGN IN</Button>
           </Nav>
         </Navbar.Collapse>
        
---
+++ frontend-react/src/Pages/LoginPage.tsx
@@ -0,0 +1,9 @@
+import Login from "../Components/login/Login"
+
+const LoginPage = () => {
+  return (
+    <Login />
+  )
+}
+
+export default LoginPage
